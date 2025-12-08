0c4c6e01b8c3e780137ddb4754edbf19050b2959
DanRCM <rcortezmanzano24@gmail.com>
2025-12-08 18:25:26 -0500

    Correcciones de renderizado

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // IMPORTANTE: Asegúrate que este nombre sea IDÉNTICO al de tu repo en GitHub
+  // Si tu repo se llama 'AquaTeam_Landing', déjalo así. 
+  // Fíjate en las mayúsculas y minúsculas.
+  base: '/AquaTeam_Landing/', 
+})
